c23b34a3453c76d4418d96f56137f28acc0e8c6e
子非鱼 <yyzusb@gmail.com>
2021-08-16 21:20:58 +0800

    笔记更新

---
+++ 08_mini_web框架/demo03_通用装饰器.py
@@ -0,0 +1,27 @@
+#通用装饰器的作用，可以装饰任意类型的函数
+
+def decorater(fn):
+    def inner(*args,**kwargs):
+        print('正在努力执行\n')
+        num = fn(*args,**kwargs)
+        return num
+    return inner
+
+@decorater
+def add_num(*args,**kwargs):
+    result = 0
+    for value in args:
+        result += value
+
+    for value in kwargs.keys():
+        result += kwargs.keys()
+
+    print('结果为:',result)
+
+add_num(44,22)
+
+
+
+
+
+
