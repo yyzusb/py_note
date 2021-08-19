b0ad7cd2b67fd6bb5d69b6e0abb40bf083787343
子非鱼 <yyzusb@gmail.com>
2021-08-19 19:08:06 +0800

    python connect redis

--- 09_redis/demo02_redis_cluster.py
+++ 09_redis/demo02_redis_cluster.py
@@ -1,12 +1,14 @@
 # encoding=utf-8
-from rediscluster import StrictRedisCluster
+import redis
+from rediscluster import RedisCluster
 
 if __name__ == '__main__':
     nodes = [{"host": "192.168.121.6", "port": "7001"},
              {"host": "192.168.121.6", "port": "7002"},
-             {"host": "192.168.121.6", "port": "6379"}, ]
+             {"host": "192.168.121.6", "port": "6379"}
+             ]
 
     try:
-        str = StrictRedisCluster(startup_nodes=nodes)
+        str = RedisCluster(startup_nodes=nodes)
     except Exception as e:
         print(e)
